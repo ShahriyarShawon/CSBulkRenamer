1a0a44acfa5736d7a5e582b61094e438b2e228b1
Shahriyar Shawon <ShahriyarShawon@gmail.com>
2020-02-29 09:53:35 -0500

    Adding Project Files

---
+++ CSBulkRenamer/BulkRenamer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSBulkRenamer
+{
+    class BulkRenamer
+    {
+
+        public static void CreateTempFile(string directoryPath, string tempFilePath)
+        {
+            string[] files_in_path = Directory.GetFiles(directoryPath);
+            StringBuilder files_as_string = new StringBuilder();
+            foreach (string file in files_in_path)
+            {
+                files_as_string.AppendLine(file);
+            }
+
+            //using (FileStream fs = File.Create("C:\\Users\\Shahr\\Desktop\\files.txt"))
+            using (FileStream fs = File.Create(tempFilePath))
+            {
+                byte[] files_as_bytes = new UTF8Encoding(true).GetBytes(files_as_string.ToString());
+                fs.Write(files_as_bytes, 0, files_as_bytes.Length);
+            }
+        }
+
+        public static void BulkRename(string tempFile, string directoryPath)
+        {
+            using(StreamReader sr = File.OpenText(tempFile))
+            {
+                string[] files_in_path = Directory.GetFiles(directoryPath);
+                for (int i = 0; i < files_in_path.Length; i++)
+                {
+                    string before = files_in_path[i];
+                    string after = sr.ReadLine();
+                    Console.WriteLine($"Moving {before} to {after}");
+                    File.Move(before,after );
+                }
+            }
+            
+
+        }
+    }
+}
---
+++ CSBulkRenamer/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSBulkRenamer;
+using CommandLine;
+
+namespace CSBulkRenamer
+{
+    class Program
+    {
+        public class Options
+        {
+            [Option('c', "command", Required = false, HelpText = "Which Command you want to run", Default = "createTempFile")]
+            public string Command { get; set; }
+
+            [Option('t', "tempFile", Required = false, HelpText = "Path to where your files.txt file is", Default = "files.txt")]
+            public string TempFile { get; set; }
+
+            [Option('d', "directoryPath", Required = false, 
+                HelpText = "Path to the directory that contains all the files you want to rename", 
+                Default = ".")]
+            public string DirectoryPath { get; set; }
+        }
+        static void Main(string [] args)
+        {
+
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed<Options>(o =>
+                {
+                    if (o.Command.Equals("createTempFile"))
+                    {
+                        BulkRenamer.CreateTempFile(o.DirectoryPath, o.TempFile);
+                    }
+                    else if (o.Command.Equals("rename"))
+                    {
+                        BulkRenamer.BulkRename(o.TempFile, o.DirectoryPath);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid options or option combinations");
+                    }
+
+                });
+
+         
+   
+
+            
+
+
+
+
+        }
+
+        
+    }
+}
